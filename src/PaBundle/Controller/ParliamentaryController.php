24bbb6e797ee64e6fc40672436bd78221d6589bd
S@int-Cyr <mapoukacyr@yahoo.fr>
2016-04-14 15:09:45 +0000

    define the variables and template

--- src/PaBundle/Controller/ParliamentaryController.php
+++ src/PaBundle/Controller/ParliamentaryController.php
@@ -9,21 +9,51 @@ class ParliamentaryController extends Controller
 {
     public function nationalAction()
     {
-        return new Response('statistics nationale level for Parliamentary');
+        //Get the ORM entity manager
+        $em = $this->getDoctrine()->getManager();
+        //Get the statisticHandler service
+        $statisticHandler = $this->get('vtally.statistic_handler');
+        //Get all the parliamentary parties
+        $parties = $em->getRepository('PaBundle:PaParty')->findAll();
+        
+        //Get all the contituencies
+        $constituencies = $em->getRepository('VtallyBundle:Constituency')->findAll();
+        //Get the sites 
+        $sites = $statisticHandler->getSiteNumber($constituencies, $parties, null);
+        //Get all the parties
+        $parties = $sites['parties'];
+        
+        //Passe the data to the view
+        return $this->render('PaBundle:VoteCast:nation.html.twig', array('parties' => $parties));
     }
     
     public function regionAction($id)
     {
-        return new Response('statistics region level for region id: '.$id);
+        //Get the ORM entity manager
+        $em = $this->getDoctrine()->getManager();
+        //Get the statisticHandler service
+        $statisticHandler = $this->get('vtally.statistic_handler');
+        //Get all the parliamentary parties
+        $parties = $em->getRepository('PaBundle:PaParty')->findAll();
+        
+        //Get all the contituencies
+        $constituencies = $em->getRepository('VtallyBundle:Constituency')->findAll();
+        //Get the sites 
+        $sites = $statisticHandler->getSiteNumber($constituencies, $parties, null);
+        //Get all the parties
+        $parties = $sites['parties'];
+        
+        //Passe the data to the view
+        return $this->render('PaBundle:VoteCast:region.html.twig', array('parties' => $parties));
     }
     
     public function constituencyAction($id)
     {
-        return new Response('statistics constituency level for constituency id: '.$id);
+        return $this->render('PaBundle:VoteCast:constituency.html.twig');
     }
     
     public function pollingStationAction($id)
     {
-        return new Response('statistics polling station level for constituency id: '.$id);
+        return $this->render('PaBundle:VoteCast:polling_station.html.twig');
     }
 }
